00358f3cab5531498ed56e8d6c9f9d97ed750ced
Kai Kevin <kevin.c.t.zhang@gmail.com>
2018-07-09 16:39:56 -0700

    extracted out fetchtoken in authentication.js

--- App.js
+++ App.js
@@ -1,38 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import fetchAuthToken from './authentication'
 export default class App extends React.Component {
   state = {
     authToken: null,
   }
 
   componentDidMount(){
-    this.fetchAuthToken().then( token => {
+    fetchAuthToken().then( token => {
       this.setState({authToken: token})
     })
-  }
-
-  async fetchAuthToken(){
-    try {
-      let response = await
-        fetch('http://littlestagings.herokuapp.com/mobile_login', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            device_type: 'ios',
-            device_token: 'deadbeef_june',
-            email: 'dispatcher2@littlefleets.com',
-            password: 'password',
-        }),
-      })
-      let responseJson = await response.json()
-      return responseJson.data.user.authentication_token
-    } catch (error) {
-      console.error(error);
-    }
   }
 
   render() {
